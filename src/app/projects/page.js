e8c62328d27956851720f702dd3cb751303a0b12
kanishkar <kanishkrish123@gmail.com>
2024-03-07 09:47:22 +0000

    Projects section changes

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -4,7 +4,15 @@ export default function Projects() {
   return (
     <section id="projects" className="projects-block">
       <div className="container">
-        <span className="title">Projects I&apos;ve done</span>
+        <span className="title">Projects Highlights</span>
+
+        <div className="wrapper">
+          <div className="item">Bitcoin-tracker</div>
+          <div className="item">Craigslist UX</div>
+          <div className="item">AES Encryption</div>
+          <div className="item">System Design</div>
+          <div className="item">Notes App</div>
+        </div>
       </div>
     </section>
   );
