2f4478d3863874575c881f667f6a79e30bd1066c
kanishkar <kanishkrish123@gmail.com>
2024-03-12 13:24:50 +0000

    Minor changes

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -3,8 +3,18 @@ import Image from "next/image";
 import { useState } from "react";
 import skillsData from "../database/skills.json";
 
+export const FILTER_SKILLS = {
+  ALL: "All",
+  LANGUAGE: "Languages",
+  DATABASES: "Databases",
+  FRAMEWORKS: "Frameworks",
+  WEBDEV: "Webdev",
+};
+
 export default function Skills() {
   const [showSkills, setShowSkills] = useState(skillsData);
+
+  const filterSkills = (type) => {};
 
   return (
     <section id="skills" className="skills-block">
@@ -12,11 +22,36 @@ export default function Skills() {
         <span className="title">Skilled at</span>
 
         <div className="skills-filter">
-          <div className="filter-item active">All</div>
-          <div className="filter-item">Languages</div>
-          <div className="filter-item">Databases</div>
-          <div className="filter-item">Frameworks</div>
-          <div className="filter-item">Webdev</div>
+          <div
+            className="filter-item active"
+            onClick={() => filterSkills(FILTER_SKILLS.ALL)}
+          >
+            {FILTER_SKILLS.ALL}
+          </div>
+          <div
+            className="filter-item"
+            onClick={() => filterSkills(FILTER_SKILLS.LANGUAGE)}
+          >
+            {FILTER_SKILLS.LANGUAGE}
+          </div>
+          <div
+            className="filter-item"
+            onClick={() => filterSkills(FILTER_SKILLS.DATABASES)}
+          >
+            {FILTER_SKILLS.DATABASES}
+          </div>
+          <div
+            className="filter-item"
+            onClick={() => filterSkills(FILTER_SKILLS.FRAMEWORKS)}
+          >
+            {FILTER_SKILLS.FRAMEWORKS}
+          </div>
+          <div
+            className="filter-item"
+            onClick={() => filterSkills(FILTER_SKILLS.WEBDEV)}
+          >
+            {FILTER_SKILLS.WEBDEV}
+          </div>
         </div>
 
         <div className="wrapper">
