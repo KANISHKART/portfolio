d10ae45c3f113838eb29733e519b6cf8fbbbd690
kanishkar <kanishkrish123@gmail.com>
2024-03-14 10:53:41 +0000

    Minor change

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -8,15 +8,22 @@ export default function Home() {
     <section id="home" className="hero-block">
       <div className="container">
         <div className="hero-greet">
-          <p> Hello! 👋</p>
-        </div>
+          <p> Hi! 👋</p>
+          <div className="hero-summary">
+            <p>
+              I&apos;m <strong>Kanishkar</strong>, an experienced software
+              engineer focused on building scalable, performant, and resilient
+              applications.🧑‍💻
+              {/* <span className="typewrite-container">Full Stack Developer</span>  */}
+            </p>
+          </div>
 
-        <div className="hero-summary">
-          <p>
-            I&apos;m <strong>Kanishkar T</strong>, an experienced software
-            engineer focused on designing & developing data intensive applications.🧑‍💻
-            {/* <span className="typewrite-container">Full Stack Developer</span>  */}
-          </p>
+          <div className="hero-connect">
+            Shoot an email 📮 : &nbsp;
+            <a href="mailto:kanishkar.t05@gmail.com" target="_blank">
+              kanishkar@gmail.com
+            </a>
+          </div>
         </div>
       </div>
     </section>
