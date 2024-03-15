681a917c55d47a34ed3f811476dceb0302fab5f0
kanishkar <kanishkrish123@gmail.com>
2024-03-15 14:35:04 +0000

    Minor changes

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -4,18 +4,18 @@ import "./home.css";
 import Image from "next/image";
 
 export default function Home() {
-  
   return (
     <section id="home" className="hero-block">
       <div className="container">
         <div className="header-container">
-          <div className="hero-background"></div>
-          <p>Kanishkar Thirunavukkarasu</p>
+          <p> Hello! 👋</p>
+        </div>
+        <div className="header-container">
+          <p> I&apos;m Kanishkar Thirunavukkarasu,</p>
         </div>
 
         <div className="role-container">
-          I&apos;m
-          <span className="typewrite-container">Full Stack Developer</span>
+          a<span className="typewrite-container">Full Stack Developer</span>
         </div>
       </div>
     </section>
