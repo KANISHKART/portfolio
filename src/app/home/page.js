2b45e402b7d9b2fad3310be539e4c8ee97222eac
kanishkar <kanishkrish123@gmail.com>
2024-03-16 12:35:01 +0000

    UI Changes

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -5,29 +5,29 @@ export default function Home() {
   return (
     <section id="home" className="hero-block">
       <div className="container">
-
         <div className="hero-greet">
           <p> Hi! 👋</p>
           <div className="hero-summary">
             <p>
               I&apos;m <strong>Kanishkar</strong>, an experienced software
               engineer focused on building scalable, performant, and resilient
-              applications.🧑‍💻
-              {/* <span className="typewrite-container">Full Stack Developer</span>  */}
+              applications 🧑‍💻
             </p>
           </div>
 
+          <div className="hero-skill">
+            <div className="skill-title">Good at :&nbsp;</div>
+            <div className="typewrite-container">Full Stack Developement</div>
+          </div>
+
           <div className="hero-connect">
-            Shoot an email 📮 : &nbsp;
+            📬 : &nbsp;
             <a href="mailto:kanishkar.t05@gmail.com" target="_blank">
               kanishkar@gmail.com
             </a>
           </div>
         </div>
-
       </div>
     </section>
-
-    
   );
 }
