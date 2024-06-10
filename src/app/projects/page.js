09c6a364e52f0332bd870c5c1bb2632f30170b7a
kanishkar <kanishkrish123@gmail.com>
2024-06-10 09:33:37 +0100

    Minor changes

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -3,7 +3,9 @@ import "./projects.css";
 export default function Projects() {
   return (
     <section id="projects" className="projects-block">
-      <div className="container">This is project section</div>
+      <div className="container">
+        <span className="title">Projects I&apos;ve done</span>
+      </div>
     </section>
   );
 }
