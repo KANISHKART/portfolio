08fd0aed7d759e0a1748af11bfab73c1c8823b19
kanishkar <kanishkrish123@gmail.com>
2024-06-16 01:14:44 +0100

    Minor changes

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -16,34 +16,36 @@ export default function Projects() {
           {projectData != null &&
             projectData.map((data, index) => {
               return (
-                <div className="item" key={`project-${data.id}-${index}`}>
-                  <div className="project-title">
-                    {data.name}
-                    <Image
-                      src="./link.svg"
-                      width={30}
-                      height={30}
-                      color="grey"
-                      alt="link"
-                    />
-                  </div>
+                <a key={`project-${data.id}-${index}`}>
+                  <div className="item">
+                    <div className="project-title">
+                      {data.name}
+                      <Image
+                        src="./link.svg"
+                        width={30}
+                        height={30}
+                        color="grey"
+                        alt="link"
+                      />
+                    </div>
 
-                  <div className="project-summary">{data.summary}</div>
+                    <div className="project-summary">{data.summary}</div>
 
-                  <div className="project-tags">
-                    {data.tags != null &&
-                      data.tags.map((tag, index) => {
-                        return (
-                          <div
-                            className="tag-value"
-                            key={`tag-${tag}-${index}`}
-                          >
-                            {tag}
-                          </div>
-                        );
-                      })}
+                    <div className="project-tags">
+                      {data.tags != null &&
+                        data.tags.map((tag, index) => {
+                          return (
+                            <div
+                              className="tag-value"
+                              key={`tag-${tag}-${index}`}
+                            >
+                              {tag}
+                            </div>
+                          );
+                        })}
+                    </div>
                   </div>
-                </div>
+                </a>
               );
             })}
         </div>
